6ce79afe9bed4569f23b40322036a4bf4abcdaea
chris60303 <chris60303@gmail.com>
2020-11-23 16:52:04 +0800

    udm debug

--- src/udm/context/context.go
+++ src/udm/context/context.go
@@ -107,6 +107,11 @@ func (context *UDMContext) ManageSmData(smDatafromUDR []models.SessionManagement
 
 	smDataMap := make(map[string]models.SessionManagementSubscriptionData)
 	sNssaiList := make([]string, len(smDatafromUDR))
+	fmt.Printf("smDataMap is %s\n\n",smDataMap)
+	fmt.Printf("len(sNssaiList) us %d\n\n",len(sNssaiList))
+	for item := range sNssaiList {
+		fmt.Printf("item is %s\n\n",item)
+	}
 	// to obtain all DNN configurations identified by "dnn" for all network slices where such DNN is available
 	AllDnnConfigsbyDnn := make([]models.DnnConfiguration, 1, len(sNssaiList))
 	// to obtain all DNN configurations for all network slice(s)
