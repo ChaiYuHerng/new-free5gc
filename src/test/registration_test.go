3bb9360e699605c9b5c6a4c794a0469eecbafa6a
chris60303 <chris60303@gmail.com>
2020-11-27 17:11:44 +0800

    tg debug

--- src/test/registration_test.go
+++ src/test/registration_test.go
@@ -48,7 +48,7 @@ var totalUdpPacket int = 120000000000
 const my_type int = 1
 const ranIpAddr string = "192.168.2.151"
 const amfIpAddr string = "192.168.2.102" // no need to change
-const upfIpAddr1 string = "192.168.2.113" // 110, 111
+const upfIpAddr1 string = "192.168.2.111" // 110, 111
 const dNServer1  string = "192.168.2.54" // 205, 206
 var dNServerI = [4]byte{192, 168, 2, 54} // 205, 206
 
