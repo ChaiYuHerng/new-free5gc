b3876769ece75e9334e9189c2b7341f7fc8ede61
chris60303 <chris60303@gmail.com>
2020-11-27 14:45:53 +0800

    tg debug

--- src/test/registration_test.go
+++ src/test/registration_test.go
@@ -46,17 +46,11 @@ var udpPacketCount int =        15000
 var totalUdpPacket int = 120000000000
 
 const my_type int = 1
-const ranIpAddr string = "192.168.2.151"
+const ranIpAddr string = "192.168.2.150"
 const amfIpAddr string = "192.168.2.102" // no need to change
-const upfIpAddr1 string = "192.168.2.111" // 110, 111
-const upfIpAddr2 string = "192.168.2.112" // 110, 111
-const upfIpAddr3 string = "192.168.2.113" // 110, 111
-const dNServer1  string = "192.168.2.54" // 205, 206
-const dNServer2  string = "192.168.2.219" // 205, 206
-const dNServer3  string = "192.168.2.23" // 205, 206
-var dNServerI = [4]byte{192, 168, 2, 54} // 205, 206
-var dNServerII = [4]byte{192, 168, 2, 219} // 205, 206
-var dNServerX = [4]byte{192, 168, 2, 23} // 205, 206
+const upfIpAddr1 string = "192.168.2.112" // 110, 111
+const dNServer1  string = "192.168.2.219" // 205, 206
+var dNServerI = [4]byte{192, 168, 2, 219} // 205, 206
 
 type UE struct {
     Supi              string
