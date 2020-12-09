fd840556fdecce3b2a524f24afd430ba4dbde35a
chris60303 <chris60303@gmail.com>
2020-12-09 16:28:02 +0800

    smf debug

--- src/smf/context/sm_context.go
+++ src/smf/context/sm_context.go
@@ -155,6 +155,7 @@ func NewSMContext(identifier string, pduSessID int32) (smContext *SMContext) {
 }
 
 func GetSMContext(ref string) (smContext *SMContext) {
+	fmt.Printf("GetSMContext test1\n\n")
 	if value, ok := smContextPool.Load(ref); ok {
 		fmt.Printf("value is %v,ok is %v\n\n",value,ok)
 		smContext = value.(*SMContext)
